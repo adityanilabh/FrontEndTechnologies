c9ead9ae585550f47c8c2daec8c8b531f38413ce
adityanilabh <nilabhaditya6@gmail.com>
2024-01-03 15:21:04 +0530

    tutorial.txt made

--- javascriptCodeFiles/fundamentals.js
+++ javascriptCodeFiles/fundamentals.js
@@ -1,2 +1,18 @@
-// javascript output
-    
+// JavaScript code
+// Step 1:
+/* Using button to change the value in file fundamentals.html */
+function thisfun() {
+    document.getElementById("h1Change").innerHTML = "Hello There! How Are You";
+}
+
+/* These below two functions will decrease and increase the value by one */
+
+function incrementByOne() {
+    let value = parseInt(document.getElementById("p1Change").innerHTML);
+    document.getElementById("p1Change").innerHTML = (value + 1).toString();
+}
+
+function decreaseByOne() {
+    let value = parseInt(document.getElementById("p1Change").innerHTML);
+    document.getElementById("p1Change").innerHTML = (value - 1).toString();
+}
