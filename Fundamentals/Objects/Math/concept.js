40835b439fba550b41537cc982f3b3c7d9f39156
adityanilabh <nilabhaditya6@gmail.com>
2024-03-18 16:54:34 +0530

    commit made

---
+++ Fundamentals/Objects/Math/concept.js
@@ -0,0 +1,20 @@
+/*Javascript Math object is used to perform mathematical operations on numbers. 
+All the properties of Math are static and unlike other objects, it does not have
+ a constructor. */
+let sqrt2= Math.SQRT2;
+console.log(sqrt2);
+
+let neg= -22;
+console.log(Math.abs(neg));
+
+let ceils= 2.223;
+console.log(Math.ceil(ceils));
+
+let floors= 22.33;
+console.log(Math.floor(floors));
+
+let randoms= Math.random();
+console.log(randoms);
+
+let mini= Math.min(1,2,34,5,3,3,3,4,3,3,33,233);
+console.log(mini);
